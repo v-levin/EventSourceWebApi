e5936d6ce0dbe7c2db4e459b82984cb756592cf7
Monika <monika_ristovska@hotmail.com>
2018-07-25 15:28:30 +0200

    add contracts

---
+++ EventSourceWebApi.Contracts/Event.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace EventSourceWebApi.Contracts
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateRegistered { get; set; }
+
+        public int Seats { get; set; }
+
+        public string Description { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int UpdatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string City { get; set; }
+
+        public string Category { get; set; }
+
+        public string Location { get; set; }
+
+        public int PlaceId { get; set; }
+
+        public bool? IsAccepted { get; set; }
+
+        public DateTime DateFrom { get; set; }
+
+        public DateTime DateTo { get; set; }
+
+        public string TimeTo { get; set; }
+
+        public string TimeFrom { get; set; }
+    }
+}
---
+++ EventSourceWebApi.Contracts/Place.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSourceWebApi.Contracts
+{
+    public class Place
+    {
+        public int Id { get; set; }
+
+        public string[] Tags { get; set; }
+
+        public string Moto { get; set; }
+
+        public DateTime DateRegistered { get; set; }
+
+        public int Capacity { get; set; }
+
+        public string Location { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int PartnerId { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int UpdatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public string City { get; set; }
+    }
+}
