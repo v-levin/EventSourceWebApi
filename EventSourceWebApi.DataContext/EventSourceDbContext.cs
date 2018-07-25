608bedec02e6468ff11ae3d15952bb31b6cda493
Monika <monika_ristovska@hotmail.com>
2018-07-25 15:34:07 +0200

    modify project

---
+++ EventSourceWebApi.DataContext/EventSourceDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EventSourceWebApi.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSourceWebApi.DataContext
+{
+    public class EventSourceDbContext : DbContext
+    {
+        public EventSourceDbContext()
+        {
+        }
+
+        public EventSourceDbContext(DbContextOptions<EventSourceDbContext> options)
+                    : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Place> Places { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
