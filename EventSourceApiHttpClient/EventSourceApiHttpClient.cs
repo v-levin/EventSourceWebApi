f43109eeda1aa1240062922d07046259f21dd55f
Monika <monika_ristovska@hotmail.com>
2018-08-06 13:20:45 +0200

    add Places client

--- EventSourceApiHttpClient/EventSourceApiHttpClient.cs
+++ EventSourceApiHttpClient/EventSourceApiHttpClient.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace EventSourceApiHttpClient
 {
     public class EventSourceApiHttpClient
     {
+
         public static void Main()
         {
 
---
+++ EventSourceApiHttpClient/PlacesClient.cs
@@ -0,0 +1,55 @@
+﻿using EventSourceWebApi.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EventSourceApiHttpClient
+{
+    public class PlacesClient
+    {
+        private readonly string url = "http://localhost:49999/places";
+        HttpClient _client= new HttpClient()
+        {
+            BaseAddress = new Uri("http://localhost:49999/"),
+            Timeout = TimeSpan.FromMilliseconds(100)
+        };
+        public void GetAllPlaces(string url)
+        {
+   
+             
+
+             
+        }
+
+        public Place GetPlace(int id)
+        {
+            Place place = null;
+            var response = _client.GetAsync($"{url}{id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responseDate = response.Content.ReadAsStringAsync().Result;
+                place = JsonConvert.DeserializeObject<Place>(responseDate);
+                return place;
+            }
+            return place;
+        }
+        public void CreatePlace()
+        {
+
+        }
+
+        public void UpdatePlace()
+        {
+
+        }
+
+        public void DeletePlace()
+        {
+
+        }
+
+
+
+    }
+}
