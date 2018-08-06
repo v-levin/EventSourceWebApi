4e6a86bac0f2320048ae9c6530d1a059323fe3a9
Monika <monika_ristovska@hotmail.com>
2018-08-06 15:36:56 +0200

    add eventSourceApiClient

--- EventSourceApiHttpClient/EventSourceApiHttpClient.cs
+++ EventSourceApiHttpClient/EventSourceApiHttpClient.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventSourceWebApi.Contracts.Requests;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
@@ -6,12 +9,22 @@ namespace EventSourceApiHttpClient
 {
     public class EventSourceApiHttpClient
     {
+        public static IConfiguration Configuration { get; set; }
 
         public static void Main()
         {
-            var client = new EventsClient();
-            var @event = client.GetEvent(4);
-            Console.WriteLine();
+
+            var eventSourceApiClient = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:49999/api"),
+                Timeout = new TimeSpan(0, 5, 0),
+            };
+            eventSourceApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var placeClient = new PlacesClient(eventSourceApiClient);
+            placeClient.GetAllPlaces(new PlaceSearchRequest() { Name = "mkc", City = "skopje" });
+            placeClient.GetPlace(38);
+
         }
     }
 }
--- EventSourceApiHttpClient/PlacesClient.cs
+++ EventSourceApiHttpClient/PlacesClient.cs
@@ -1,31 +1,40 @@
 ﻿using EventSourceWebApi.Contracts;
+using EventSourceWebApi.Contracts.Requests;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace EventSourceApiHttpClient
 {
     public class PlacesClient
     {
-        private readonly string url = "http://localhost:49999/places";
-        HttpClient _client= new HttpClient()
+        private readonly HttpClient client;
+        private const string placesRootUrl = "places";
+        public PlacesClient(HttpClient _client)
         {
-            BaseAddress = new Uri("http://localhost:49999/"),
-            Timeout = TimeSpan.FromMilliseconds(100)
-        };
-        public void GetAllPlaces(string url)
+            client = _client;
+        }
+        public IEnumerable<Place> GetAllPlaces(PlaceSearchRequest request)
         {
-   
-             
+            var response = client.GetAsync($"{client.BaseAddress}/{placesRootUrl}?name={request.Name}&city={request.City}&location={request.Location}").Result;
+            var places = new List<Place>();
 
-             
+            if (response.IsSuccessStatusCode)
+            {
+                var responseDate = response.Content.ReadAsStringAsync().Result;
+                places = JsonConvert.DeserializeObject<List<Place>>(responseDate);
+                return places;
+            }
+            return places;
         }
 
         public Place GetPlace(int id)
         {
             Place place = null;
-            var response = _client.GetAsync($"{url}{id}").Result;
+            var response = client.GetAsync($"{client.BaseAddress}/{id}").Result;
             if (response.IsSuccessStatusCode)
             {
                 var responseDate = response.Content.ReadAsStringAsync().Result;
@@ -34,9 +43,13 @@ namespace EventSourceApiHttpClient
             }
             return place;
         }
-        public void CreatePlace()
+        public void CreatePlace(HttpRequestMessage place)
         {
+            HttpResponseMessage response = client.PostAsync($"{client.BaseAddress}places", place.Content).Result;
+            response.EnsureSuccessStatusCode();
 
+          
+                    
         }
 
         public void UpdatePlace()
@@ -48,7 +61,6 @@ namespace EventSourceApiHttpClient
         {
 
         }
-
 
 
     }
