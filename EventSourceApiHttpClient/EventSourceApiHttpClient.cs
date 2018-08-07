c4357dbd1e14f8e764ea5b52068c3fe0464eadf2
Viktor Levin <viktor.levin@it-labs.com>
2018-08-07 15:14:23 +0200

    modify code

--- EventSourceApiHttpClient/EventSourceApiHttpClient.cs
+++ EventSourceApiHttpClient/EventSourceApiHttpClient.cs
@@ -8,19 +8,22 @@ namespace EventSourceApiHttpClient
     {
         public static void Main()
         {
-            var eventClient = new EventsClient("http://localhost:49999/api/", "application/json"); 
+            var baseUrl = "http://localhost:49999/api/";
+            var acceptHeader = "application/json";
 
-            var placeClient = new PlacesClient("http://localhost:49999/api/", "application/json");
+            var eventClient = new EventsClient(baseUrl, acceptHeader); 
+
+            var placeClient = new PlacesClient(baseUrl, acceptHeader);
 
             var placeTest = new Test(placeClient);
             placeTest.Run();
 
 
-            //var getEvents = client.GetEvents(new EventSearchRequest() { City = "skopje" });
-            //var getEvent = client.GetEvent(new EventIdRequest() { Id = 16 });
-            //var post = client.PostEvent(new Event() { Name = "Http Client", Seats = 400, Description = "Http Client Desc", City = "Skopje", Category = "Art", Location = "Central Park", DateRegistered = DateTime.Now });
-            //var put = client.PutEvent(26, new Event() { Id = 26, City = "Kumanovo", Description = "Desc Photo", Category = "Art", Seats = 80 });
-            //var delete = client.DeleteEvent(25);
+            //var getEvents = eventClient.GetEvents(new EventSearchRequest() { City = "skopje" });
+            //var getEvent = eventClient.GetEvent(new EventIdRequest() { Id = 16 });
+            //var post = eventClient.PostEvent(new Event() { Name = "Http Client", Seats = 400, Description = "Http Client Desc", City = "Skopje", Category = "Art", Location = "Central Park", DateRegistered = DateTime.Now });
+            //var put = eventClient.PutEvent(26, new Event() { Id = 26, City = "Kumanovo", Description = "Desc Photo", Category = "Art", Seats = 80 });
+            //var delete = eventClient.DeleteEvent(25);
         }
     }
 }
