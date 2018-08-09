f285962b85f337d24fe5958f270e55df5feaf587
Monika <monika_ristovska@hotmail.com>
2018-08-09 14:33:55 +0200

    add logger in PlacesDurableFunction

--- EventSourceApi.Functions/PlacesDurableFunction.cs
+++ EventSourceApi.Functions/PlacesDurableFunction.cs
@@ -2,6 +2,8 @@ using EventSourceApiHttpClient;
 using EventSourceWebApi.Contracts;
 using EventSourceWebApi.Contracts.Requests;
 using Microsoft.Azure.WebJobs;
+using Serilog;
+using Serilog.Core;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ namespace EventSourceApi.Functions
         private const string baseUrl = "http://localhost:49999/api/";
         private const string mediaType = "application/json";
         private static BaseHttpClient client = new BaseHttpClient(baseUrl, mediaType);
+        private static readonly Logger log = new LoggerConfiguration().WriteTo.Console().WriteTo.File("log.txt").CreateLogger();
 
         [FunctionName("PlacesDurableFunction")]
         public static async Task RunAsync(
@@ -32,7 +35,7 @@ namespace EventSourceApi.Functions
 
             var updatePlace = await context.CallActivityAsync<Place>("UpdatePlace", placeId);
 
-            var isDeletePlace = await context.CallActivityAsync<HttpStatusCode>("DeletePlace", updatePlace.Id);
+            var isDeletedPlace = await context.CallActivityAsync<HttpStatusCode>("DeletePlace", updatePlace.Id);
 
             var getPlace = await context.CallActivityAsync<Place>("GetPlace", placeId);
         }
@@ -40,39 +43,75 @@ namespace EventSourceApi.Functions
         [FunctionName("CreatePlace")]
         public static int? CreatePlace([ActivityTrigger] Place place)
         {
-            var request = new PostRequest<Place>() { Payload = place };
+            log.Information("Calling CreatePlace function");
+            try
+            {
+                var request = new PostRequest<Place>() { Payload = place };
 
-            return client.PlacesClient.PostPlace(request);
+                return client.PlacesClient.PostPlace(request);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                return null;
+            }
         }
 
         [FunctionName("UpdatePlace")]
         public static Place UpdatePlace([ActivityTrigger] int placeId)
         {
-            var newPlace = new Place()
+            log.Information($"Calling UpdatePlace function for Place with Id: {placeId}");
+            try
             {
-                Description = "Update Place...",
-                City = "Radovis"
-            };
+                var newPlace = new Place()
+                {
+                    Description = "Update Place...",
+                    City = "Radovis"
+                };
 
-            var request = new PutRequest<Place>() { Id = placeId, Payload = newPlace };
+                var request = new PutRequest<Place>() { Id = placeId, Payload = newPlace };
 
-            return client.PlacesClient.PutPlace(request);
+                return client.PlacesClient.PutPlace(request);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                return null;
+            }
         }
 
         [FunctionName("DeletePlace")]
         public static bool DeletePlace([ActivityTrigger] int placeId)
         {
-            var request = new PlaceIdRequest() { Id = placeId };
+            log.Information($"Calling DeletePlace function for Place with Id: {placeId}");
+            try
+            {
+                var request = new PlaceIdRequest() { Id = placeId };
 
-            return client.PlacesClient.DeletePlace(request);
+                return client.PlacesClient.DeletePlace(request);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                return false;
+            }
         }
 
         [FunctionName("GetPlace")]
         public static Place GetPlace([ActivityTrigger] int placeId)
         {
-            var request = new PlaceIdRequest() { Id = placeId };
+            log.Information($"Calling GetPlace function with palceId : {placeId}");
+            try
+            {
+                var request = new PlaceIdRequest() { Id = placeId };
 
-            return client.PlacesClient.GetPlace(request);
+                return client.PlacesClient.GetPlace(request);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                return null;
+            }
         }
     }
 }
