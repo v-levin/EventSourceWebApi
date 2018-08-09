3085d0636a59a009d2ba2490f346d4d649230552
Monika <monika_ristovska@hotmail.com>
2018-08-09 11:01:51 +0200

    add HttpStart

--- EventSourceApi.Functions/PlacesDurableFunction.cs
+++ EventSourceApi.Functions/PlacesDurableFunction.cs
@@ -1,5 +1,8 @@
+using EventSourceApiHttpClient;
+using EventSourceWebApi.Contracts;
 using Microsoft.Azure.WebJobs;
-using System.Collections.Generic;
+using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace EventSourceApi.Functions
@@ -7,12 +10,74 @@ namespace EventSourceApi.Functions
     public static class PlacesDurableFunction
     {
         [FunctionName("PlacesDurableFunction")]
-        public static void Run(
+        public static async Task RunAsync(
             [OrchestrationTrigger] DurableOrchestrationContextBase context)
         {
-          
+            var place = new Place()
+            {
+                Name = "place1",
+                Description = "Wdefrgb",
+                Capacity = 70,
+                Location = "Radovis",
+                DateRegistered = DateTime.Now,
+                City = "Skopje"
+            };
 
+            int createPlaceId = await context.CallActivityAsync<int>("CreatePlace", place);
+
+            Place updatePlace = await context.CallActivityAsync<Place>("UpdatePlace", createPlaceId);
+
+            HttpStatusCode deletePlace = await context.CallActivityAsync<HttpStatusCode>("DeletePlace", updatePlace.Id);
+
+            Place getPlace = await context.CallActivityAsync<Place>("GetPlace", deletePlace);
         }
 
+        public static BaseHttpclient IntiClient()
+        {
+            var baseUrl = "http://localhost:49999/api/";
+            var mediaType = "application/json";
+            return new BaseHttpclient(baseUrl, mediaType);
+        }
+
+        [FunctionName("CreatePlace")]
+        public static int? CreatePlace([ActivityTrigger] Place place)
+        {
+            var client = IntiClient();
+            var placeId = client.PlacesClient.PostPlace(place);
+            return placeId;
+        }
+
+        [FunctionName("UpdatePlace")]
+        public static Place UpdatePlace([ActivityTrigger] int placeId)
+        {
+            var newPlace = new Place()
+            {
+                Name = "place1",
+                Description = "Wdefrgb",
+                Capacity = 70,
+                Location = "Radovis",
+                DateRegistered = DateTime.Now,
+                City = "Radovis"
+            };
+            var client = IntiClient();
+            var place = client.PlacesClient.PutPlace(placeId, newPlace);
+            return place;
+        }
+
+        [FunctionName("DeletePlace")]
+        public static HttpStatusCode DeletePlace([ActivityTrigger] int placeId)
+        {
+            var client = IntiClient();
+            var place = client.PlacesClient.DeletePlace(placeId);
+            return place;
+        }
+
+        [FunctionName("GetPlace")]
+        public static Place GetPlace([ActivityTrigger] int placeId)
+        {
+            var client = IntiClient();
+            var place = client.PlacesClient.GetPlace(placeId);
+            return place;
+        }
     }
 }
