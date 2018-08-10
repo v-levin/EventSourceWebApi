b08e6b6155bd9ea86f7eaf062afddc4b6738e151
Viktor Levin <viktor.levin@it-labs.com>
2018-08-10 13:30:40 +0200

    modify durable function

--- EventSourceEvents.Functions/HttpStart.cs
+++ EventSourceEvents.Functions/HttpStart.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using EventSourceWebApi.Contracts;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Serilog;
 using Serilog.Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EventSourceEvents.Functions
@@ -21,16 +19,15 @@ namespace EventSourceEvents.Functions
 
         [FunctionName("HttpStart")]
         public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "orchestrators/{functionName}")] HttpRequestMessage req,
-            [OrchestrationClient] DurableOrchestrationClientBase starter,
-            string functionName)
+            [HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = EventsConstants.Route)] HttpRequestMessage req,
+            [OrchestrationClient] DurableOrchestrationClientBase starter)
         {
             // Function input comes from the request content.
-            dynamic eventData = await req.Content.ReadAsAsync<object>();
-            string instanceId = await starter.StartNewAsync(functionName, eventData);
+            var eventData = await req.Content.ReadAsAsync<Event>();
+            string instanceId = await starter.StartNewAsync(EventsConstants.FunctionName, eventData);
 
             log.Information($"Started orchestration with ID = '{instanceId}'.");
-            var tt = req.Headers;
+
             var res = starter.CreateCheckStatusResponse(req, instanceId);
             res.Headers.RetryAfter = new RetryConditionHeaderValue(TimeSpan.FromSeconds(10));
             return res;
